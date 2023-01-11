f571226f0a4f0179a19fa8e9303a8b299b89f961
Mundra-1997 <darshandm28@gmail.com>
2023-01-12 01:05:03 +0530

    commit

--- index.js
+++ index.js
@@ -37,7 +37,29 @@ var swiper = new Swiper(".product-slider", {
     loop:true,
     spaceBetween: 20,
     autoplay:{
-        delay:7500,
+        delay:5500,
+        disableOnInteraction:false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        
+      },
+      768: {
+        slidesPerView: 2,
+       
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  var swiper = new Swiper(".review-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay:{
+        delay:5500,
         disableOnInteraction:false,
     },
     centeredSlides: true,
