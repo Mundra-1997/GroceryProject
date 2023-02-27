9e7d4608081653f75cc11268fdad22fd1e6d7569
Mundra-1997 <darshandm28@gmail.com>
2023-02-27 23:32:47 +0530

    commit

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ document.querySelector('#cart-btn').addEventListener('click',function(){
 
     }
     else{
+     
       shoppingCart1.classList.toggle('active');
+      
+      calculate();
           if(qtOnion>0){
             //  document.getElementById('showonion').innerText = `${qtOnion}`
 
@@ -151,48 +154,71 @@ var swiper = new Swiper(".product-slider", {
   let qtCarrot = 0;
   let qtAvocado = 0;
   let qtCabbage = 0;
+  function nowhere(){
+    if(qtAvocado==0 && qtCabbage==0 && qtCarrot==0 && qtOnion==0 && qtOrange==0 && qtPatato==0){
+      
+      shoppingCart.classList.toggle('active');
 
+    }
+  }
+  nowhere();
+  
+  function showCartvalueonTop(){
+    document.getElementById('cartnumber').innerText=qtAvocado+qtCabbage+qtCarrot+qtOnion+qtOrange+qtPatato;
+  }
   
 
 
   document.getElementById('orange1').addEventListener('click',()=>{
     flag++;
     qtOrange++;
+    showCartvalueonTop();
     console.log(qtOrange);
+    
+
     
   })
   document.getElementById('onion2').addEventListener('click',()=>{
     qtOnion++;
     flag++;
+    showCartvalueonTop();
     console.log(qtOnion);
   })
   document.getElementById('cabbage3').addEventListener('click',()=>{
     qtCabbage++;
     flag++;
+    showCartvalueonTop();
     console.log(qtCabbage);
   })
   document.getElementById('patatoes4').addEventListener('click',()=>{
     qtPatato++;
     flag++;
+    showCartvalueonTop();
     console.log(qtPatato);
   })
   document.getElementById('avocados5').addEventListener('click',()=>{
     qtAvocado++;
     flag++;
+    showCartvalueonTop();
     console.log(qtAvocado);
   })
   document.getElementById('carrots6').addEventListener('click',()=>{
     qtCarrot++;
     flag++;
+    showCartvalueonTop();
     console.log(qtCarrot);
   })
 
 document.getElementById('increonion').addEventListener('click',()=>{
   qtOnion++;
   update();
+  showCartvalueonTop();
+  calculate();
 })
 document.getElementById('decreonion').addEventListener('click',()=>{
   qtOnion--;
+  calculate();
+  showCartvalueonTop();
   if(qtOnion===0){
     document.getElementById('cartonion').classList.add('box');
     document.getElementById('cartonion').classList.remove('doggo');
@@ -203,10 +229,14 @@ document.getElementById('decreonion').addEventListener('click',()=>{
 })
 document.getElementById('increcarrot').addEventListener('click',()=>{
   qtCarrot++;
+  showCartvalueonTop();
   update();
+  calculate();
 })
 document.getElementById('decrecarrot').addEventListener('click',()=>{
   qtCarrot--;
+  calculate();
+  showCartvalueonTop();
   if(qtCarrot===0){
     document.getElementById('cartcarrot').classList.add('box');
     document.getElementById('cartcarrot').classList.remove('doggo');
@@ -217,10 +247,14 @@ document.getElementById('decrecarrot').addEventListener('click',()=>{
 })
 document.getElementById('increcabbage').addEventListener('click',()=>{
   qtCabbage++;
+  showCartvalueonTop();
+  calculate();
   update();
 })
 document.getElementById('decrecabbage').addEventListener('click',()=>{
   qtCabbage--;
+  showCartvalueonTop();
+  calculate();
   if(qtCabbage===0){
     document.getElementById('cartcabbage').classList.add('box');
     document.getElementById('cartcabbage').classList.remove('doggo');
@@ -231,10 +265,14 @@ document.getElementById('decrecabbage').addEventListener('click',()=>{
 })
 document.getElementById('increpatato').addEventListener('click',()=>{
   qtPatato++;
+  calculate();
   update();
+  showCartvalueonTop();
 })
 document.getElementById('decrepatato').addEventListener('click',()=>{
   qtPatato--;
+  showCartvalueonTop();
+  calculate();
   if(qtPatato===0){
     document.getElementById('cartpatato').classList.add('box');
     document.getElementById('cartpatato').classList.remove('doggo');
@@ -245,10 +283,14 @@ document.getElementById('decrepatato').addEventListener('click',()=>{
 })
 document.getElementById('increavocado').addEventListener('click',()=>{
   qtAvocado++;
+  showCartvalueonTop();
+  calculate();
   update();
 })
 document.getElementById('decreavocado').addEventListener('click',()=>{
   qtAvocado--;
+  showCartvalueonTop();
+  calculate();
   if(qtAvocado===0){
     document.getElementById('cartavocado').classList.add('box');
     document.getElementById('cartavocado').classList.remove('doggo');
@@ -259,10 +301,15 @@ document.getElementById('decreavocado').addEventListener('click',()=>{
 })
 document.getElementById('increorange').addEventListener('click',()=>{
   qtOrange++;
+  showCartvalueonTop();
+  calculate();
   update();
+
 })
 document.getElementById('decreorange').addEventListener('click',()=>{
   qtOrange--;
+  showCartvalueonTop();
+  calculate();
   if(qtOrange===0){
     document.getElementById('cartorange').classList.add('box');
     document.getElementById('cartorange').classList.remove('doggo');
@@ -271,7 +318,10 @@ document.getElementById('decreorange').addEventListener('click',()=>{
   
   update();
 })
-document.getElementById('calculate').addEventListener('click',()=>{
+function calculate(){
   let total = priceAvocado*qtAvocado + priceCabbage*qtCabbage + priceCarrot*qtCarrot + priceOnion*qtOnion + priceOrange*qtOrange + pricePatato*qtPatato;
   document.getElementById('bill').innerText = `Rs - ${total}`;
+}
+document.getElementById('calculate').addEventListener('click',()=>{
+           calculate();
 })
